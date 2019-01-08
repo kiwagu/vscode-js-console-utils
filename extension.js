0402dd98212a0c6c57db7fa39a3e19892e787890
kiwagu <kiwagu@gmail.com>
2019-01-08 13:14:17 +0600

    CHANGED: improve log output format

--- extension.js
+++ extension.js
@@ -1,90 +1,107 @@
-const vscode = require('vscode');
+const vscode = require("vscode")
 
 const insertText = (val) => {
-    const editor = vscode.window.activeTextEditor;
+  const editor = vscode.window.activeTextEditor
 
-    if (!editor) {
-        vscode.window.showErrorMessage('Can\'t insert log because no document is open');
-        return;
-    }
+  if (!editor) {
+    vscode.window.showErrorMessage(
+      "Can't insert log because no document is open",
+    )
+    return
+  }
 
-    const selection = editor.selection;
+  const selection = editor.selection
 
-    const range = new vscode.Range(selection.start, selection.end);
+  const range = new vscode.Range(selection.start, selection.end)
 
-    editor.edit((editBuilder) => {
-        editBuilder.replace(range, val);
-    });
+  editor.edit((editBuilder) => {
+    editBuilder.replace(range, val)
+  })
 }
 
 function getAllLogStatements(document, documentText) {
-    let logStatements = [];
+  let logStatements = []
 
-    const logRegex = /console.(log|debug|info|warn|error|assert|dir|dirxml|trace|group|groupEnd|time|timeEnd|profile|profileEnd|count)\((.*)\);?/g;
-    let match;
-    while (match = logRegex.exec(documentText)) {
-        let matchRange =
-            new vscode.Range(
-                document.positionAt(match.index),
-                document.positionAt(match.index + match[0].length)
-            );
-        if (!matchRange.isEmpty)
-            logStatements.push(matchRange);
-    }
-    return logStatements;
+  const logRegex = /console.(log|debug|info|warn|error|assert|dir|dirxml|trace|group|groupEnd|time|timeEnd|profile|profileEnd|count)\((.*)\);?/g
+  let match
+  while ((match = logRegex.exec(documentText))) {
+    let matchRange = new vscode.Range(
+      document.positionAt(match.index),
+      document.positionAt(match.index + match[0].length),
+    )
+    if (!matchRange.isEmpty) logStatements.push(matchRange)
+  }
+  return logStatements
 }
 
 function deleteFoundLogStatements(workspaceEdit, docUri, logs) {
-    logs.forEach((log) => {
-        workspaceEdit.delete(docUri, log);
-    });
+  logs.forEach((log) => {
+    workspaceEdit.delete(docUri, log)
+  })
 
-    vscode.workspace.applyEdit(workspaceEdit).then(() => {
-        logs.length > 1
-            ? vscode.window.showInformationMessage(`${logs.length} console.logs deleted`)
-            : vscode.window.showInformationMessage(`${logs.length} console.log deleted`);
-    });
+  vscode.workspace.applyEdit(workspaceEdit).then(() => {
+    logs.length > 1
+      ? vscode.window.showInformationMessage(
+          `${logs.length} console.logs deleted`,
+        )
+      : vscode.window.showInformationMessage(
+          `${logs.length} console.log deleted`,
+        )
+  })
 }
 
 function activate(context) {
-    console.log('console-log-utils is now active');
+  console.log("console-log-utils is now active")
 
-    const insertLogStatement = vscode.commands.registerCommand('extension.insertLogStatement', () => {
-        const editor = vscode.window.activeTextEditor;
-        if (!editor) { return; }
+  const insertLogStatement = vscode.commands.registerCommand(
+    "extension.insertLogStatement",
+    () => {
+      const editor = vscode.window.activeTextEditor
+      if (!editor) {
+        return
+      }
 
-        const selection = editor.selection;
-        const text = editor.document.getText(selection);
+      const selection = editor.selection
+      const text = editor.document.getText(selection)
 
-        text
-            ? vscode.commands.executeCommand('editor.action.insertLineAfter')
-                .then(() => {
-                    const logToInsert = `console.log("===========LOGSSTART===========")\nconsole.log('${text}: ', ${text})\nconsole.log("============LOGSEND============")`
-                    insertText(logToInsert);
-                })
-            : insertText('console.log();');
+      text
+        ? vscode.commands
+            .executeCommand("editor.action.insertLineAfter")
+            .then(() => {
+              const logToInsert = `console.debug(\n    \"===LOGSSTART===\\n\",\n    \">${Date.now()
+                .toString()
+                .substring(
+                  7,
+                )}<\\n\",\n    \"${text}: ↴\\n",\n    ${text},\n    \"\\n====LOGSEND====\\n",\n  )`
+              insertText(logToInsert)
+            })
+        : insertText("console.debug()")
+    },
+  )
+  context.subscriptions.push(insertLogStatement)
 
-    });
-    context.subscriptions.push(insertLogStatement);
+  const deleteAllLogStatements = vscode.commands.registerCommand(
+    "extension.deleteAllLogStatements",
+    () => {
+      const editor = vscode.window.activeTextEditor
+      if (!editor) {
+        return
+      }
 
-    const deleteAllLogStatements = vscode.commands.registerCommand('extension.deleteAllLogStatements', () => {
-        const editor = vscode.window.activeTextEditor;
-        if (!editor) { return; }
+      const document = editor.document
+      const documentText = editor.document.getText()
 
-        const document = editor.document;
-        const documentText = editor.document.getText();
+      let workspaceEdit = new vscode.WorkspaceEdit()
 
-        let workspaceEdit = new vscode.WorkspaceEdit();
+      const logStatements = getAllLogStatements(document, documentText)
 
-        const logStatements = getAllLogStatements(document, documentText);
-
-        deleteFoundLogStatements(workspaceEdit, document.uri, logStatements);
-    });
-    context.subscriptions.push(deleteAllLogStatements);
+      deleteFoundLogStatements(workspaceEdit, document.uri, logStatements)
+    },
+  )
+  context.subscriptions.push(deleteAllLogStatements)
 }
-exports.activate = activate;
+exports.activate = activate
 
-function deactivate() {
-}
+function deactivate() {}
 
-exports.deactivate = deactivate;
+exports.deactivate = deactivate
